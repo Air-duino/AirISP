a26b619095b3181503326bddd947f9b0029c910f
HalfSweet <HalfSweet@HalfSweet.cn>
2023-03-10 20:49:39 +0800

    fix: 增加了命令行参数提示功能
    加入了一点小小的微软的命令行参数解析库的震撼，并模仿esptool的参数风格编写

--- AirISP/Program.cs
+++ AirISP/Program.cs
@@ -1,43 +1,124 @@
 ﻿using System.Diagnostics;
 using System.IO.Ports;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using System.Reflection;
+using System.IO;
 
 namespace AirISP
 {
     internal class Program
     {
+        public struct BaseParameter //命令前的参数
+        {
+            public string chip;
+            public string port;
+            public string baud;
+            public bool trace;
+            public int connectAttempts;
+        }
+
+        public struct WriteFlashParameter //write_flash的参数
+        {
+            public string address;
+            public string filename;
+        }
+
         static int Main(string[] args)
         {
-            //固件路径 设备串口 [起始地址默认0x0800 0000]
-            var fwPath = args[0];
-            var portPath = args[1];
-            var baseAddress = Convert.ToUInt32((args.Length >= 3 ? args[2] : "0x08000000").Substring(2),16);
+            // 创建根命令
+            var rootCommand = new RootCommand("AirISP is a tool for flash programming.");
 
-            Console.WriteLine($"download {Path.GetFileName(fwPath)} to {portPath} from 0x{baseAddress:X} ...");
+            // 添加通用参数
+            var chip = new Option<string>("--chip", "Target chip type, optional auto,air001");
+            chip.AddAlias("-c");
+            var port = new Option<string>("--port", "Serial port device");
+            port.AddAlias("-p");
+            var baud = new Option<int>("--baud", "Serial port baud rate used when flashing/reading");
+            baud.AddAlias("-b");
+            var trace = new Option<bool>("--trace", () => false, "Enable trace-level output of AirISP interactions.");
+            trace.AddAlias("-t");
+            var connect_attempts = new Option<int>("--connect-attempts", () => 10, "Number of attempts to connect, negative or 0 for infinite. Default: 10");
+            rootCommand.Add(chip);
+            rootCommand.Add(port);
+            rootCommand.Add(baud);
+            rootCommand.Add(trace);
+            rootCommand.Add(connect_attempts);
+
+            // 创建 write_flash 命令
+            var writeFlashCommand = new Command("write_flash", "Write firmware to flash");
+            // 添加命令参数
+            var writeFlashAddress = new Argument<string>(name: "address", description: "0x00");
+            var writeFlashFilename = new Argument<string>("filename");
+
+            // 添加命令选项
+            var writeFlashEarseAll = new Option<bool>("--erase-all", "Erase all sectors on flash before writing (default only erase sectors to be written)");
+            writeFlashEarseAll.AddAlias("-e");
+            var writeFlashNoProgress = new Option<bool>("--no-progress", "Disable progress bar printing");
+            writeFlashNoProgress.AddAlias("-p");
+
+            writeFlashCommand.Add(writeFlashAddress);
+            writeFlashCommand.Add(writeFlashFilename);
+            writeFlashCommand.Add(writeFlashEarseAll);
+            writeFlashCommand.Add(writeFlashNoProgress);
+
+            //rootCommand.AddCommand(writeFlashCommand);
+
+            // 设置 write_flash 命令的处理器
+            writeFlashCommand.SetHandler((baseParm, writeFlashParm) =>
+            {
+                //TODO
+            },
+            new BinderBaseParameter(chip, port, baud, trace, connect_attempts),
+            new BinderWriteFlashParameter(writeFlashAddress, writeFlashFilename, writeFlashEarseAll, writeFlashNoProgress));
+
+            // 将 write_flash 命令添加到根命令中
+            rootCommand.AddCommand(writeFlashCommand);
+
+            // 解析并执行命令行参数
+            rootCommand.InvokeAsync(args);
+
+
+            return 0;
+        }
+
+        public static bool WriteFlash(string chip, string serialPort, int baud, bool trace, int connect_attempts, string address, string filename, bool earseAll, bool noProgress)
+        {
+            Console.WriteLine($"AirISP v{Assembly.GetExecutingAssembly().GetName().Version}");
+            Console.WriteLine($"Serial port {serialPort}");
+            Console.WriteLine("Connect...");
 
             //读取固件数据
-            var data = File.ReadAllBytes(fwPath);
+            var data = File.ReadAllBytes(address[0]);
 
-            //串口
-            var port = new SerialPort(portPath, 115200, Parity.Even, 8, StopBits.One);
+            //打开串口
+            var port = new SerialPort(serialPort, baud, Parity.Even, 8, StopBits.One);
             port.Open();
 
-            //重启设备
-            Console.WriteLine($"try to reset device to boot status ...");
-            var resetResult = false;
-            for(int i=0;i<10;i++)
+            if (port.IsOpen == false)
             {
-                Console.WriteLine($"try to reset device via cts and dtr ({i+1}/10) ...");
-                if(TryReset(port,true))
+                Console.WriteLine("Serial port opening failed!!!");
+                return false;
+            }
+
+            //重启设备
+            var resetResult = false;
+            for (int i = 0; i < connect_attempts; i++)
+            {
+                Console.WriteLine($"try to reset device via rts and dtr ({i + 1}/10) ...");
+                if (TryReset(port, true))
                 {
                     resetResult = true;
                     break;
                 }
             }
             //自动重启失败，等待手动重启进boot
-            if(!resetResult)
+            if (!resetResult)
             {
                 Console.WriteLine($"fail to reset device to boot status, please reset device manually ...");
-                for (int i=0;i<10;i++)
+                for (int i = 0; i < 10; i++)
                 {
                     Console.WriteLine($"waitting ({i + 1}/10) ...");
                     if (TryReset(port))
@@ -52,7 +133,7 @@ namespace AirISP
             if (!resetResult)
             {
                 Console.WriteLine($"fail to reset device to boot status, timeout, exit");
-                return 1;
+                return false;
             }
 
             //擦除数据
@@ -64,7 +145,7 @@ namespace AirISP
                 //进不去，可能读保护开了，关掉
                 Console.WriteLine($"{retry(port, new byte[] { 0x92, 0x6d }, 0x79, 5, 100)}");
                 Console.WriteLine("enter erase mode failed, please retry later");
-                return 2;
+                return false;
             }
             //全擦地址
             if (retry(port, new byte[] { 0xff, 0xff, 0x00 }, 0x79, 1, 100))
@@ -72,7 +153,7 @@ namespace AirISP
             else
             {
                 Console.WriteLine("erase failed, please retry later");
-                return 3;
+                return false;
             }
 
             //处理下固件数据
@@ -130,22 +211,20 @@ namespace AirISP
                 }
 
                 now += 0x80;
-                Console.WriteLine($"downloading... {(double)(now- baseAddress) / (latestAddr - baseAddress) *100:f2}%");
+                Console.WriteLine($"downloading... {(double)(now - baseAddress) / (latestAddr - baseAddress) * 100:f2}%");
             }
 
 
             //正常重启进入运行模式
             Console.WriteLine($"all done, reboot.");
             port.DtrEnable = true;
-            retry(port,new byte[] { 0x21, 0xde }, 0x79, 5, 100);
+            retry(port, new byte[] { 0x21, 0xde }, 0x79, 5, 100);
             retry(port, new byte[] { 0x08, 0, 0, 0, 0x08 }, 0x79, 1, 0);
             port.RtsEnable = true;
             Thread.Sleep(200);
             port.RtsEnable = false;
             Thread.Sleep(200);
-            return 0;
         }
-
 
         /// <summary>
         /// 重启进入boot模式
@@ -153,10 +232,10 @@ namespace AirISP
         /// <param name="port">串口对象</param>
         /// <param name="uart_ctrl">是否使用硬件流控进入重启？rts#连reset，dtr#连boot</param>
         /// <returns></returns>
-        public static bool TryReset(SerialPort port,bool uart_ctrl = false)
+        public static bool TryReset(SerialPort port, bool uart_ctrl = false)
         {
             //串口芯片控制尝试进入boot模式
-            if(uart_ctrl)
+            if (uart_ctrl)
             {
                 port.DtrEnable = false;
                 port.RtsEnable = true;
@@ -191,7 +270,7 @@ namespace AirISP
                 }
 
                 //可能错开了1字节，补上试试
-                serial.Write(new byte[] {0x00}, 0, 1);
+                serial.Write(new byte[] { 0x00 }, 0, 1);
             }
             return false;
         }
